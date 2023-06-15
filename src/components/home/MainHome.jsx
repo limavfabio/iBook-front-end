8b6dc744e7be027244bdb19a98b8235b25ad3608
JakariaJishan <jakariajishan987@gmail.com>
2023-06-16 02:17:59 +0600

    Fix linters offenses

--- src/components/home/MainHome.jsx
+++ src/components/home/MainHome.jsx
@@ -1,48 +1,48 @@
-import React from 'react'
+import React from 'react';
 
 const MainHome = () => {
-    const products = [
-        {
-            title: 'vehicle1',
-            img:'https://vespaindia.com/images/classic/vxl_yellow.webp',
-            desc:'blablabal hello world this is vehicle where you can drive into the mood. hahah',
-            bgColor:'lime'
-        },
-        {
-            title: 'vehicle2',
-            img:'https://vespaindia.com/images/classic/vxl_yellow.webp',
-            desc:'blablabal hello world this is vehicle where you can drive into the mood. hahah',
-            bgColor:'gray'
-        },
-        {
-            title: 'vehicle3',
-            img:'https://vespaindia.com/images/classic/vxl_yellow.webp',
-            desc:'blablabal hello world this is vehicle where you can drive into the mood. hahah',
-            bgColor:'green'
-        },
-    ]
+  const products = [
+    {
+      title: 'vehicle1',
+      img: 'https://vespaindia.com/images/classic/vxl_yellow.webp',
+      desc: 'blablabal hello world this is vehicle where you can drive into the mood. hahah',
+      bgColor: 'lime',
+    },
+    {
+      title: 'vehicle2',
+      img: 'https://vespaindia.com/images/classic/vxl_yellow.webp',
+      desc: 'blablabal hello world this is vehicle where you can drive into the mood. hahah',
+      bgColor: 'gray',
+    },
+    {
+      title: 'vehicle3',
+      img: 'https://vespaindia.com/images/classic/vxl_yellow.webp',
+      desc: 'blablabal hello world this is vehicle where you can drive into the mood. hahah',
+      bgColor: 'green',
+    },
+  ];
   return (
-    <div className='text-center w-full mx-auto'>
-        <h2 className='font-black text-2xl uppercase'>Latest Models</h2>
-        <p className='text-[#D2D2D2] text-xs font-bold'>Please select a vespa model</p>
-        <div className="md:flex  gap-5 mt-16 items-center justify-center">
-           {products.map(product => (
-                <div key={product.title} className='flex-1'>
-                    <div  className={` w-[200px] h-[200px] rounded-full flex justify-center items-center mx-auto `}>
-                         <img src={product.img} alt="img" className="rounded-full" />
-                    </div>
-                    <h2 className="my-5  font-bold">{product.title}</h2>
-                    <p className='text-[#D2D2D2] mb-10'>{product.desc}</p>
-                </div>
-            ))}
-            <div className='ml-auto hidden md:block'>
-                <button className='bg-[#97BF0F] w-full pl-3 pr-5 lg:pr-8 py-3 rounded-l-full'>
-                <img width="24" height="24" src="https://img.icons8.com/material-outlined/24/play--v1.png" alt="play--v1"/>
-                </button>
+    <div className="text-center w-full mx-auto">
+      <h2 className="font-black text-2xl uppercase">Latest Models</h2>
+      <p className="text-[#D2D2D2] text-xs font-bold">Please select a vespa model</p>
+      <div className="md:flex  gap-5 mt-16 items-center justify-center">
+        {products.map((product) => (
+          <div key={product.title} className="flex-1">
+            <div className=" w-[200px] h-[200px] rounded-full flex justify-center items-center mx-auto ">
+              <img src={product.img} alt="img" className="rounded-full" />
             </div>
+            <h2 className="my-5  font-bold">{product.title}</h2>
+            <p className="text-[#D2D2D2] mb-10">{product.desc}</p>
+          </div>
+        ))}
+        <div className="ml-auto hidden md:block">
+          <button type="button" className="bg-[#97BF0F] w-full pl-3 pr-5 lg:pr-8 py-3 rounded-l-full">
+            <img width="24" height="24" src="https://img.icons8.com/material-outlined/24/play--v1.png" alt="play--v1" />
+          </button>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default MainHome
+export default MainHome;
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -4,13 +4,13 @@ import MainHome from '../components/home/MainHome';
 function Home() {
   return (
     <div className="flex gap-5">
-      <div className='flex-none'>
+      <div className="flex-none">
         <h1>Home</h1>
         <p>This is the home page</p>
         <Link to="/about">Link to the about page</Link>
       </div>
-      <div className=' w-3/4 mx-auto'>
-       <MainHome />
+      <div className=" w-3/4 mx-auto">
+        <MainHome />
       </div>
     </div>
   );
