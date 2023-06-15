8b6e8b5d8cfd28a1849d2e0319db4de6cf2d7883
limavfabio <92121463+limavfabio@users.noreply.github.com>
2023-06-15 10:30:23 -0300

    Rename router

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { RouterProvider } from 'react-router-dom';
 
-import router from './AppRouter';
+import router from './router';
 import Header from './components/layout/Header';
 
 function App() {
