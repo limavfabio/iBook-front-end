884129f580288a139728edd3c196546e35748b95
limavfabio <92121463+limavfabio@users.noreply.github.com>
2023-06-15 15:28:33 -0300

    Initial details page design

--- src/routes/Details.jsx
+++ src/routes/Details.jsx
@@ -1,5 +1,25 @@
 function Details() {
-  return <div>This is the details page</div>;
+  return (
+    <div className="flex w-full">
+      {/* Left Side Full Screen Image */}
+      <div className="w-full">Left side image of package</div>
+      {/* Right Side Details Panel */}
+      <div className="w-1/3 min-w-fit bg-red-100 px-5 text-end">
+        <h2 className="text-3xl">Package name</h2>
+        <p className="text-sm">Package description</p>
+        <div className="flex justify-between text-sm">
+          <p>Package price</p>
+          <p>$10.00</p>
+        </div>
+        <div className="flex justify-between text-sm">
+          <p>Duration</p>
+          <p>1 Hour</p>
+        </div>
+        <p>DISCOVER MORE MODELS</p>
+        <button>Configure</button>
+      </div>
+    </div>
+  );
 }
 
 export default Details;
