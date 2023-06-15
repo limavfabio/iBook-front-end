fc2cfc9ef4738a99438f826ffe50ac0eaaf65f0f
JakariaJishan <jakariajishan987@gmail.com>
2023-06-16 02:16:13 +0600

    Design home page

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,11 +1,17 @@
 import { Link } from 'react-router-dom';
+import MainHome from '../components/home/MainHome';
 
 function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-      <p>This is the home page</p>
-      <Link to="/about">Link to the about page</Link>
+    <div className="flex gap-5">
+      <div className='flex-none'>
+        <h1>Home</h1>
+        <p>This is the home page</p>
+        <Link to="/about">Link to the about page</Link>
+      </div>
+      <div className=' w-3/4 mx-auto'>
+       <MainHome />
+      </div>
     </div>
   );
 }
