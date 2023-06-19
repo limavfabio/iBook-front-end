f5677c178d2574994e72b9b568d07633a6c9d9ee
limavfabio <92121463+limavfabio@users.noreply.github.com>
2023-06-19 13:49:46 -0300

    Style details Page

--- src/routes/Details.jsx
+++ src/routes/Details.jsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
 
+import DetailRow from "../components/DetailRow";
+import { ChevronRightIcon } from "@heroicons/react/24/outline";
+import ConfigureButton from "../components/ConfigureButton";
 import Header from "../components/layout/Header";
 import Sidebar from "../components/layout/Sidebar";
 import MobileSidebar from "../components/layout/MobileSidebar";
@@ -18,23 +21,35 @@ function Details() {
       <div className="flex">
         <Sidebar />
         {isSidebarOpen && <MobileSidebar />}
-        <div className="flex w-full">
+        <div className="flex w-full items-center">
           {/* Left Side Full Screen Image */}
-          <div className="w-full">Left side image of package</div>
+          <div className="w-full">
+            <img
+              src="https://vespaindia.com/images/classic/vxl_yellow.webp"
+              alt="Vespa"
+            />
+          </div>
           {/* Right Side Details Panel */}
-          <div className="w-1/3 min-w-fit bg-red-100 px-5 text-end">
-            <h2 className="text-3xl">Package name</h2>
-            <p className="text-sm">Package description</p>
-            <div className="flex justify-between text-sm">
-              <p>Package price</p>
-              <p>$10.00</p>
+          <div className="flex h-full w-1/3 min-w-fit flex-col justify-center px-5 text-end">
+            <h2 className="mb-1 text-4xl">Package name</h2>
+            <p className="mb-5">Package description</p>
+            {/* Table */}
+            <div className="mb-5 flex flex-col justify-between">
+              <DetailRow
+                label={"Package price"}
+                value={"$150.00"}
+                isGrayBackground
+              />
+              <DetailRow label={"Date"} value={"May 15"} />
+              <DetailRow label={"Start Time"} value={"9:00"} isGrayBackground />
+              <DetailRow label={"End Time"} value={"11:00"} />
+              <DetailRow label={"Duration"} value={"1 Hour"} isGrayBackground />
             </div>
-            <div className="flex justify-between text-sm">
-              <p>Duration</p>
-              <p>1 Hour</p>
-            </div>
-            <p>DISCOVER MORE MODELS</p>
-            <button type="button">Configure</button>
+            <p className="flex items-center self-end font-bold">
+              DISCOVER MORE MODELS{" "}
+              <ChevronRightIcon className="h-5 text-[#97BF0F]" />{" "}
+            </p>
+            <ConfigureButton className="mt-5" />
           </div>
         </div>
       </div>
