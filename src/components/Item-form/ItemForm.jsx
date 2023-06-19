f3790149e8d709a2cda611cbe4a5e9076d7ec211
limavfabio <92121463+limavfabio@users.noreply.github.com>
2023-06-19 17:19:29 -0300

    Form adds a new item
    Using builtin fetch function

--- src/components/Item-form/ItemForm.jsx
+++ src/components/Item-form/ItemForm.jsx
@@ -2,25 +2,51 @@ import React, { useState } from 'react';
 
 const ItemForm = () => {
   const [name, setName] = useState('');
-  const [photo, setPhoto] = useState('');
-  const [vehicle, setVehicle] = useState('');
-  const [rate, setRate] = useState('');
+  const [description, setDescription] = useState('');
+  const [image, setImage] = useState('');
+  const [price, setPrice] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setName('');
-    setPhoto('');
-    setVehicle('');
-    setRate('');
+
+    const data = {
+      name,
+      description,
+      image,
+      price,
+      owner_id: 1,
+    };
+
+    fetch('http://localhost:3001/api/v1/products', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        // Handle the successful response
+        console.log('Product created:', result);
+        // Reset the form fields
+        setName('');
+        setDescription('');
+        setImage('');
+        setPrice('');
+      })
+      .catch((error) => {
+        // Handle the error
+        console.error('Error creating product:', error);
+      });
   };
 
   return (
-    <div className="flex h-screen w-full flex-col items-center justify-center bg-[#E4E5E9] container">
+    <div className="container flex h-screen w-full flex-col items-center justify-center bg-[#E4E5E9]">
       <form
         onSubmit={handleSubmit}
-        className="border border-[#9CA3AF] py-10 px-8 shadow-2xl bg-white"
+        className="border border-[#9CA3AF] bg-white px-8 py-10 shadow-2xl"
       >
-        <h2 className=" font-bold uppercase text-center">Add New Item</h2>
+        <h2 className=" text-center font-bold uppercase">Add New Item</h2>
         <div className="w-72 md:w-96">
           <label htmlFor="name" className="mb-2 block">
             <p className="font-semibold">Name</p>
@@ -29,47 +55,47 @@ const ItemForm = () => {
               id="name"
               value={name}
               onChange={(e) => setName(e.target.value)}
-              placeholder="Fabio Lima"
-              className="my-2 w-full border border-[#BFD872] px-6 py-2 focus:outline-none focus:bg-white focus:border-[#85a80d]"
+              placeholder="John Doe"
+              className="my-2 w-full border border-[#BFD872] px-6 py-2 focus:border-[#85a80d] focus:bg-white focus:outline-none"
               required
             />
           </label>
-          <label htmlFor="photo" className="mb-2 block">
-            <p className="font-semibold">Photo</p>
+          <label htmlFor="description" className="mb-2 block">
+            <p className="font-semibold">Description</p>
 
             <input
               type="text"
-              id="photo"
-              value={photo}
+              id="description"
+              value={description}
+              placeholder="A nice marker kit"
+              onChange={(e) => setDescription(e.target.value)}
+              className="my-2 w-full border border-[#BFD872] px-6 py-2 focus:border-[#85a80d] focus:bg-white focus:outline-none"
+              required
+            />
+          </label>
+          <label htmlFor="image" className="mb-2 block">
+            <p className="font-semibold">Image</p>
+
+            <input
+              type="text"
+              id="image"
+              value={image}
               placeholder="https://vespaindia.com/images/classic/vxl_yellow.webp"
-              onChange={(e) => setPhoto(e.target.value)}
-              className="my-2 w-full border border-[#BFD872] px-6 py-2 focus:outline-none focus:bg-white focus:border-[#85a80d]"
+              onChange={(e) => setImage(e.target.value)}
+              className="my-2 w-full border border-[#BFD872] px-6 py-2 focus:border-[#85a80d] focus:bg-white focus:outline-none"
               required
             />
           </label>
-          <label htmlFor="vehicle" className="mb-2 block">
-            <p className="font-semibold">Vehicle</p>
+          <label htmlFor="price" className="mb-2 block">
+            <p className="font-semibold">Price</p>
 
             <input
               type="text"
-              id="vehicle"
-              value={vehicle}
-              placeholder="Audi A8"
-              onChange={(e) => setVehicle(e.target.value)}
-              className="my-2 w-full border border-[#BFD872] px-6 py-2 focus:outline-none focus:bg-white focus:border-[#85a80d]"
-              required
-            />
-          </label>
-          <label htmlFor="rate" className="mb-2 block">
-            <p className="font-semibold">Rate</p>
-
-            <input
-              type="number"
-              id="rate"
-              value={rate}
-              placeholder="5"
-              onChange={(e) => setRate(e.target.value)}
-              className="my-2 w-full border border-[#BFD872] px-6 py-2 focus:outline-none focus:bg-white focus:border-[#85a80d]"
+              id="price"
+              value={price}
+              placeholder="Price"
+              onChange={(e) => setPrice(e.target.value)}
+              className="my-2 w-full border border-[#BFD872] px-6 py-2 focus:border-[#85a80d] focus:bg-white focus:outline-none"
               required
             />
           </label>
@@ -80,10 +106,10 @@ const ItemForm = () => {
             Submit
           </button>
           <button
-            type="submit"
-            className="mt-4 ml-5 bg-[#E4E5E9] px-6 py-2 text-black hover:bg-[#cacacc] focus:bg-[#9a9a9c]"
+            type="button"
+            className="ml-5 mt-4 bg-[#E4E5E9] px-6 py-2 text-black hover:bg-[#cacacc] focus:bg-[#9a9a9c]"
           >
-            Cancle
+            Cancel
           </button>
         </div>
       </form>
