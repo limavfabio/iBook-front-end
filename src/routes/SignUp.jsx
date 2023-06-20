2eb3aab74f71f92a33b268d3e5714c99112ef7ac
limavfabio <92121463+limavfabio@users.noreply.github.com>
2023-06-20 19:38:18 -0300

    Style login and sign up components

--- src/routes/SignUp.jsx
+++ src/routes/SignUp.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { createUser } from "../redux/userSlice";
 
@@ -21,7 +21,7 @@ function SignUp() {
       const userExists = users.some((user) => user.username === username);
 
       if (userExists) {
-        setErrorMessage("User already exists");
+        setErrorMessage("User already exists, please log in.");
         return;
       } else {
         // Dispatch the createUser action to store the username
@@ -35,34 +35,36 @@ function SignUp() {
   };
 
   return (
-    <div>
-      <h1>Signup page</h1>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="username" className="mb-2 block">
-          <p className="font-semibold">Username</p>
-
-          <input
-            type="text"
-            id="username"
-            value={username}
-            placeholder="Enter your username"
-            onChange={(e) => setUsername(e.target.value)}
-            className="my-2 border border-[#BFD872] px-6 py-2 focus:border-[#85a80d] focus:bg-white focus:outline-none"
-            required
-          />
-        </label>
-
-        {errorMessage && (
-          <p className="text-sm font-semibold text-red-500">{errorMessage}</p>
-        )}
-
-        <button
-          type="submit"
-          className="mt-4 bg-[#97BF0F] px-6 py-2 text-white hover:bg-[#85a80d] focus:bg-[#6d8a0a]"
-        >
-          Submit
-        </button>
-      </form>
+    <div className='h-screen flex flex-col bg-slate-100 items-center justify-center'>
+      <div className='border p-5 rounded-lg bg-white shadow w-min'>
+        <form onSubmit={handleSubmit} className='flex flex-col items-end'>
+          <label htmlFor="username" className="mb-2 block">
+            <p className="font-semibold text-center text-slate-700 pb-3">Sign up with your username</p>
+            <input
+              type="text"
+              id="username"
+              value={username}
+              placeholder="Enter your username"
+              onChange={(e) => setUsername(e.target.value)}
+              className="my-2 border border-[#BFD872] px-6 py-2 focus:border-[#85a80d] focus:bg-white focus:outline-none"
+              required
+            />
+          </label>
+          {errorMessage && (
+            <p className="text-sm font-semibold text-red-500">{errorMessage}</p>
+          )}
+          <button
+            type="submit"
+            className="mt-2 bg-[#97BF0F] px-6 py-2 text-white hover:bg-[#85a80d] focus:bg-[#6d8a0a]"
+          >
+            Sign Up
+          </button>
+        </form>
+        <div className='flex flex-col items-center text-slate-700'>
+          <p className='mt-4'>Already have an username?</p>
+          <Link to='/login' className='hover:text-black hover:underline' >Click here to log in.</Link>
+        </div>
+      </div>
     </div>
   );
 }
