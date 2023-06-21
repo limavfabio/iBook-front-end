5b20cd5b59433d54bdab6221f6825b674310978d
limavfabio <92121463+limavfabio@users.noreply.github.com>
2023-06-21 09:38:55 -0300

    Create products page

--- src/routes/Products.jsx
+++ src/routes/Products.jsx
@@ -1,5 +1,95 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { fetchProducts } from '../redux/productsSlice';
+import Header from '../components/layout/Header';
+import Sidebar from '../components/layout/Sidebar';
+import MobileSidebar from '../components/layout/MobileSidebar';
+
 function Products() {
-  return <h1>Products</h1>
+  // Fetch data from the store
+  const products = useSelector((state) => state.products.value.products);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, [dispatch]);
+
+  // State for sidebar
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
+  function handleDelete(id) {
+    fetch(`http://localhost:3000/api/v1/products/${id}`, {
+      method: 'DELETE',
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Success:', data);
+        dispatch(fetchProducts());
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }
+
+  return (
+    <>
+      <Header toggleSidebar={toggleSidebar} />
+      <div className="flex">
+        <Sidebar />
+        {isSidebarOpen && <MobileSidebar />}
+        {/* Table */}
+        <table className="min-w-full divide-y divide-gray-200">
+          <thead>
+            <tr>
+              <th className="px-6 py-3 bg-gray-100 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                ID
+              </th>
+              <th className="px-6 py-3 bg-gray-100 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Name
+              </th>
+              <th className="px-6 py-3 bg-gray-100 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Price
+              </th>
+              <th className="px-6 py-3 bg-gray-100 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Actions
+              </th>
+            </tr>
+          </thead>
+          <tbody className="bg-white divide-y divide-gray-200">
+            {products ? (
+              products.map((product) => (
+                <tr key={product.id}>
+                  <td className="px-6 py-4 whitespace-nowrap">{product.id}</td>
+                  <td className="px-6 py-4 whitespace-nowrap">{product.name}</td>
+                  <td className="px-6 py-4 whitespace-nowrap">{product.price}</td>
+                  <td className="px-6 py-4 whitespace-nowrap">
+                    <button
+                      onClick={() => handleDelete(product.id)}
+                      className="px-3 py-1 text-sm font-medium text-red-500 bg-transparent border border-red-500 rounded-md hover:bg-red-500 hover:text-white"
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))
+            ) : (
+              <tr>
+                <td colSpan="4" className="px-6 py-4 text-center">
+                  No products found.
+                </td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
 }
 
 export default Products;
