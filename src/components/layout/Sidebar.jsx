afd09eb51cfb8e6d2c1168563fc8f0b0b878dfaa
limavfabio <92121463+limavfabio@users.noreply.github.com>
2023-06-21 10:53:07 -0300

    Route based bg-lime

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -1,23 +1,48 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { useLogout } from '../../lib/auth';
 
 function Sidebar() {
   const logout = useLogout();
+  const location = useLocation();
 
   return (
-    <div className=" hidden h-screen w-3/4 flex-col border border-gray-100 bg-white sm:w-1/2 md:static md:flex lg:w-1/4">
-      <h2 className="mt-8 self-center text-xl">Vespa</h2>
+    <div className=" hidden h-screen min-w-max pr-5 w-3/4 flex-col border border-gray-100 bg-white sm:w-1/2 md:static md:flex lg:w-1/4">
+      <h2 className="mt-8 self-center text-xl">VenomPrecision</h2>
       <ul className="mt-16">
-        <li className="ml-2 bg-[#97BF0F] py-2 pl-3 font-bold text-white">
-          MODELS
-        </li>
-        <Link to='/reservations'>
-          <li className="ml-2 py-2 pl-3 font-bold text-slate-700">MY RESERVATIONS</li>
+        <Link to="/">
+          <li
+            className={`ml-2 py-2 pl-3 font-bold text-slate-700 ${
+              location.pathname === '/' ? 'bg-lime-500' : ''
+            }`}
+          >
+            MAIN PAGE
+          </li>
+        </Link>
+        <Link to="/reservations">
+          <li
+            className={`ml-2 py-2 pl-3 font-bold text-slate-700 ${
+              location.pathname === '/reservations' ? 'bg-lime-500' : ''
+            }`}
+          >
+            MY RESERVATIONS
+          </li>
         </Link>
         <Link to="/products">
-          <li className="ml-2 py-2 pl-3 font-bold text-slate-700">ALL PRODUCTS</li>
+          <li
+            className={`ml-2 py-2 pl-3 font-bold text-slate-700 ${
+              location.pathname === '/products' ? 'bg-lime-500' : ''
+            }`}
+          >
+            ALL PRODUCTS
+          </li>
         </Link>
-        <button type="button" className="ml-2 py-2 pl-3 font-bold text-slate-700 hover:cursor-pointer" onClick={logout}>LOGOUT</button>
+        <button
+          type="button"
+          className="ml-2 py-2 pl-3 font-bold text-slate-700 hover:cursor-pointer"
+          onClick={logout}
+        >
+          LOGOUT
+        </button>
       </ul>
       <ul className="mt-auto flex">
         <li>icon</li>
