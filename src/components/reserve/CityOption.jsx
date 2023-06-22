3b4aa5f3344027ecb0b3c3085f08965429b7855c
limavfabio <92121463+limavfabio@users.noreply.github.com>
2023-06-22 13:01:39 -0300

    Fix accessibility issues on CityOption

--- src/components/reserve/CityOption.jsx
+++ src/components/reserve/CityOption.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const CityOption = ({ city, openCity }) => (
-  <li
-    // role="button"
-    // tabIndex={0}
-    className="cursor-pointer px-4 py-2 hover:bg-[#97BF0F] hover:text-white"
-    onClick={() => openCity(city)}
-  >
-    {city}
+  <li className="w-full">
+    <button
+      type="button"
+      className="cursor-pointer text-start w-full px-4 py-2 hover:bg-[#97BF0F] hover:text-white"
+      onClick={() => openCity(city)}
+    >
+      {city}
+    </button>
   </li>
 );
 
