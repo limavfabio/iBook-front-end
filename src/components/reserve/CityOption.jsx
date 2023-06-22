027af9768e6536d94945975927494e847162aa83
limavfabio <92121463+limavfabio@users.noreply.github.com>
2023-06-22 12:45:51 -0300

    Add PropTypes for CityOption

--- src/components/reserve/CityOption.jsx
+++ src/components/reserve/CityOption.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const CityOption = ({ city, openCity }) => (
   <li
-    role="button"
-    tabIndex={0}
+    // role="button"
+    // tabIndex={0}
     className="cursor-pointer px-4 py-2 hover:bg-[#97BF0F] hover:text-white"
     onClick={() => openCity(city)}
   >
     {city}
   </li>
 );
+
+CityOption.propTypes = {
+  city: PropTypes.string.isRequired,
+  openCity: PropTypes.func.isRequired,
+};
 
 export default CityOption;
