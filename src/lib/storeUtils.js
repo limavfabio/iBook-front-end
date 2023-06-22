0c0e946867af8812c0d1d4582f65dd75153b01cd
limavfabio <92121463+limavfabio@users.noreply.github.com>
2023-06-22 12:25:29 -0300

    Delete uneccessary function

--- src/lib/storeUtils.js
+++
@@ -1,6 +0,0 @@
-import { useSelector } from 'react-redux';
-
-/* eslint-disable import/prefer-default-export */
-export function getUsernameFromStore() {
-  return useSelector((state) => state.user.username);
-}
