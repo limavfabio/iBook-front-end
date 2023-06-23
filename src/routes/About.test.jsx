bf4d0c21c15c34b9bcbce8e30549e529427fbfad
JakariaJishan <jakariajishan987@gmail.com>
2023-06-24 04:11:10 +0600

    Recover two delelted files

---
+++ src/routes/About.test.jsx
@@ -0,0 +1,19 @@
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import store from '../redux/store';
+import About from './About';
+
+describe('Main Home', () => {
+  it('expects to render a snapshot of the Home component', () => {
+
+    const episodeTest = render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <About/>
+        </BrowserRouter>
+      </Provider>,
+    );
+    expect(episodeTest).toMatchSnapshot();
+  });
+});
